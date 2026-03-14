8857c887405ce60cf04de2ff531074469c624129
JSeHoone <beebea545@gmail.com>
2026-03-14 21:39:05 +0900

    fix : call back url 하드코딩 제거

--- app/application/dto/dto.py
+++ app/application/dto/dto.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
+from app.application.container import settings
 
+CALLBACK_URL = settings.call_back_url
 class ExtractedExperienceMessage(BaseModel):
     id: int
     user_id: int
     file_asset_id: int
     job_type: str = "EXPERIENCE_EXTRACTION" # 작업 유형을 나타내는 필드 추가
-    callback_url: str | None = "http://host.docker.internal:8080/api/v1/callbacks/experience_extraction" # TODO : 실제 콜백 URL로 변경하기
-
+    callback_url: str | None = CALLBACK_URL + "experience_extraction"
